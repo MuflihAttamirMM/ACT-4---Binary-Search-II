1bae799ade3b3a27089952a33e27f701ae7612d4
Muflay <muflihattamirmuslim@gmail.com>
2026-04-17 14:51:33 +0700

    First Commit

---
+++ BinarySearch.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int element{10};
+int nPanjang;
+int x;
+
+void input()
+{
+    while (true)
+    {
+        cout << "masukkan banyaknya elemen pada array (maksimal 10) : ";
+        cin >> nPanjang;
+
+    }
+}
