90393e6080b45c1cbc2eb3dcb3f2978a7aa5ecd3
Muflay <muflihattamirmuslim@gmail.com>
2026-04-17 15:01:02 +0700

    melengkapi cout

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -25,4 +25,11 @@ void input()
         cout << "\n===============================\n";
         cout << "   Masukkan Elemen Array         \n";
         cout << "\n===============================\n";
+    
+    for (int i = 0; i < nPanjang; i++)
+    {
+        cout << "Data ke-" << (i + 1) << " = ";
+        cin >> element[i];
+    }
 }
+
